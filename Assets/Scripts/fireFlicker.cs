74c72500e64d91e20e9ce7d1dba27ad287a4504d
Dante <dante@oomba.com>
2015-12-02 09:23:07 -0800

    Changes

---
+++ Assets/Scripts/fireFlicker.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fireFlicker : MonoBehaviour {
+
+    public Light fireLight;
+    public float timeOn = 0.3f;
+    public float timeOff = 0.3f;
+    private float changeTime = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Time.time > changeTime){
+            //fireLight.enabled = !fireLight.enabled;
+            if (fireLight.intensity < 4.0f)
+            {
+                fireLight.intensity = 4.0f;
+                changeTime = Time.time + timeOff;
+            }
+            else
+            {
+                fireLight.intensity = 3.5f;
+                changeTime = Time.time + timeOn;
+            }
+        }
+	}
+}
---
+++ Assets/Scripts/whatAreThose.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class whatAreThose : MonoBehaviour {
+
+    private AudioSource sound;
+    private bool inRange = false;
+	// Use this for initialization
+	void Start () {
+        sound = this.GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (inRange)
+        {
+            if (Input.GetKeyDown("e"))
+            {
+                sound.Play();
+            }     
+        }
+	}
+
+    void OnTriggerEnter(Collider obj)
+    {
+        inRange = true;
+    }
+    void OnTriggerExit(Collider obj)
+    {
+        inRange = false;
+    }
+}
