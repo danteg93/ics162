a50380c0111fddf970ebea168c88138ffe558858
Dante <dante@oomba.com>
2015-12-04 14:44:50 -0800

    did a bunch of changes

--- Assets/Scripts/fireFlicker.cs
+++ Assets/Scripts/fireFlicker.cs
@@ -7,23 +7,25 @@ public class fireFlicker : MonoBehaviour {
     public float timeOn = 0.3f;
     public float timeOff = 0.3f;
     private float changeTime = 0;
-	// Use this for initialization
+    public float intesityAddition = 0.3f;
+    private float lightFactor;
+        // Use this for initialization
 	void Start () {
-	
+        lightFactor = fireLight.intensity + intesityAddition;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (Time.time > changeTime){
             //fireLight.enabled = !fireLight.enabled;
-            if (fireLight.intensity < 4.0f)
+            if (fireLight.intensity < lightFactor)
             {
-                fireLight.intensity = 4.0f;
+                fireLight.intensity = lightFactor;
                 changeTime = Time.time + timeOff;
             }
             else
             {
-                fireLight.intensity = 3.5f;
+                fireLight.intensity = fireLight.intensity - intesityAddition;
                 changeTime = Time.time + timeOn;
             }
         }
