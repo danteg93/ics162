06a2882a75f5aa6ac12247fb686975d1bd0efefb
Dante <dante@oomba.com>
2015-12-04 16:09:56 -0800

    deleted some assets and added some gui to the shoes

--- Assets/Scripts/whatAreThose.cs
+++ Assets/Scripts/whatAreThose.cs
@@ -5,9 +5,14 @@ public class whatAreThose : MonoBehaviour {
 
     private AudioSource sound;
     private bool inRange = false;
+    private int w = 200;
+    private int h = 80;
+    private Rect squareThing;
+    
 	// Use this for initialization
 	void Start () {
         sound = this.GetComponent<AudioSource>();
+        squareThing = new Rect((Screen.width - w) / 2, (Screen.height - h) / 2, w, h);
 	}
 	
 	// Update is called once per frame
@@ -20,7 +25,13 @@ public class whatAreThose : MonoBehaviour {
             }     
         }
 	}
-
+    void OnGUI()
+    {
+        if (inRange)
+        {
+            GUI.Box(squareThing, ("\nPress E to interact with\nShoes?"));
+        }
+    }
     void OnTriggerEnter(Collider obj)
     {
         inRange = true;
