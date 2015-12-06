10e8eff0410d84be1cf5119b57f0e1f388aab1f0
DANTE GARCIA RAMIREZ <dante@oomba.com>
2015-12-05 16:12:37 -0800

    Added teporter and thats it

---
+++ Assets/Scripts/portal.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class portal : MonoBehaviour {
+
+	public GameObject otherPortal;
+	static bool teleporting = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider obj){
+		Debug.Log("TELEPORT DAMN IT");
+		if(!teleporting){
+			teleporting = true;
+			obj.gameObject.transform.position = otherPortal.transform.position;
+		}
+	}
+	void OnTriggerExit(Collider obj){
+		Debug.Log("TELEPORT DAMN IT");
+		if(teleporting){
+			teleporting = false;
+		}
+	}
+}
